1a3fa7e33d10316e11c58a20a43ba4f087af378a
Andreas Tiefenthaler <at@an-ti.eu>
2013-12-17 09:32:23 +0100

    adding trainer

--- lib/trainer.rb
+++ lib/trainer.rb
@@ -1,14 +1,31 @@
 class Trainer
+  attr_reader :graph
 
-  def train(graph, sentences)
+
+  def initialize(graph)
+    @graph = graph || Graph.new
+  end
+
+  def train(sentences)
     sentences.each do |sentence|
-      words(sentence).each do |word|
-
+      words = sentence_to_words(sentence)
+      words.each_with_index do |word, index|
+        next_word = words[index+1]
+        add_word_to_graph(word, next_word)
       end
     end
   end
 
-  def words(sentence)
+  def add_word_to_graph(word,next_word)
+    if next_word
+      n1 = make_node(word)
+      n2 = make_node(next_word)
+      edge = Edge.new(n1,n2)
+      @graph.add_edge(edge)
+    end
+  end
+
+  def sentence_to_words(sentence)
     sentence.split(' ')
   end
 
